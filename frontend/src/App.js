0e46c5e44a71200fffaae3ca88fba3ae02e803a0
tatvkamdar79 <kamdartatv79@gmail.com>
2023-03-07 11:53:09 +0530

    Signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-import Routing from "./Views/Routing";
+import Home from "./Views/Home";
+import Signup from "./Views/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Routing from "./Views/Routing";
 
 function App() {
-  return <Routing />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/home"} element={<Home />} />
+        <Route path={"/signup"} element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/Views/Routing.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Home from "./Home";
-
-const Routing = () => {
-  return <Home />;
-};
-
-export default Routing;
