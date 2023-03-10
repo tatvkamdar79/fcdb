124e8b8238e62be11a1a74df8f402fedc4fe45d4
Aman Jolly <aman.amanjolly@gmail.com>
2023-03-10 13:42:01 +0530

    Mongoose Setup

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+app.set("port", PORT);
+
+// Parsing the request parameters
+app.use(express.urlencoded());
+app.use(express.static('./assests'));
+
+//Set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+//Use express router 
+app.use('/',require('./routes/index.js'));
+
+app.listen(PORT, () => {
+  console.log("Yay");
+});
