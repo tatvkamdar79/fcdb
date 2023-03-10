63342763a53d5690fcb796490790763532890446
Tatva Kamdar <kamdartatv1@gmail.com>
2023-03-10 14:30:26 +0530

    client signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./Views/Home";
 import Signup from "./Views/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClientSignup from "./Views/ClientSignup";
 // import Routing from "./Views/Routing";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path={"/"} element={<Home />} />
         <Route path={"/home"} element={<Home />} />
         <Route path={"/signup"} element={<Signup />} />
+        <Route path={"/signup/client"} element={<ClientSignup />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Views/ClientSignup.jsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const ClientSignup = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const changeName = (e) => {
+    setName(e.target.value);
+  };
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const headers = {
+      "Content-Type": "text/plain",
+    };
+
+    console.log(name, password);
+    const url = "http://localhost:8080/client/sign-up";
+    await axios
+      .post(url, { name: name, password: password }, { headers })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  return (
+    <div>
+      hi
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">User Name</label>
+        <input
+          key="1"
+          className="border-2 border-black rounded-md px-1"
+          type="text"
+          name="name"
+          onChange={changeName}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          key="2"
+          className="border-2 border-black rounded-md px-1"
+          type="password"
+          name="password"
+          onChange={changePassword}
+        />
+
+        <input type="submit" value="Submit" />
+      </form>
+      <div>
+        <p>{name}</p>
+        <p>{password}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ClientSignup;
