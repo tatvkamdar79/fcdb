188b059f44fdbdd32974f848ca7e3321a7f6b3f5
hrithik <hrithikjainDB@gmail.com>
2023-03-13 23:32:13 +0530

    RandomPush

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -3,24 +3,41 @@ import React from 'react'
 
 export const Card = ({ id, ServiceName, ImageLink, RedirectLink,Description}) => {
   return (
-    <a href="/">
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#"> 
-            <img class="rounded-t-lg" src={ImageLink} alt="" />
-        </a>
-        <div class="p-5">
-            <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ServiceName}</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{Description}</p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Click here to know more!
-        </p>
-        </div>
-        
-    </div>
+    <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
 
-    </a>
+    <article class="overflow-hidden rounded-lg shadow-lg">
+            <a href="/">
+                <img alt="Placeholder" class="block h-auto w-full" src={ImageLink}/>
+            </a>
+
+            <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                <h1 class="text-lg">
+                    <a class="no-underline hover:underline text-black" href="/">
+                        {ServiceName}
+                    </a>
+                </h1>
+                <p class="text-grey-darker text-sm">
+                    11/1/19
+                </p>
+            </header>
+
+            <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                <a class="flex items-center no-underline hover:underline text-black" href="/">
+                    
+                    <p class="ml-2 text-sm">
+                        {Description}
+                    </p>
+                </a>
+                <a class="no-underline text-grey-darker hover:text-red-dark" href="/">
+                    <span class="hidden">Like</span>
+                    <i class="fa fa-heart"></i>
+                </a>
+            </footer>
+
+            </article>
+            </div>        
+    
+    
     
   )
 }
--- frontend/src/Components/MultipleCards.jsx
+++ frontend/src/Components/MultipleCards.jsx
@@ -1,45 +1,16 @@
 import React from 'react'
+import { Card } from './Card'
 
 export const MultipleCards = ({ id, ServiceName, ImageLink, RedirectLink,Description}) => {
   return (
     <div>
       <div class="container my-12 mx-auto px-4 md:px-12">
           <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-
-            <article class="overflow-hidden rounded-lg shadow-lg">
-
-                  <a href="/">
-                      <img alt="Placeholder" class="block h-auto w-full" src="https://picsum.photos/600/400/?random"/>
-                  </a>
-
-                  <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                      <h1 class="text-lg">
-                          <a class="no-underline hover:underline text-black" href="/">
-                              Article Title
-                          </a>
-                      </h1>
-                      <p class="text-grey-darker text-sm">
-                          11/1/19
-                      </p>
-                  </header>
-
-                  <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                      <a class="flex items-center no-underline hover:underline text-black" href="/">
-                          <img alt="Placeholder" class="block rounded-full" src="https://picsum.photos/32/32/?random"/>
-                          <p class="ml-2 text-sm">
-                              Author Name
-                          </p>
-                      </a>
-                      <a class="no-underline text-grey-darker hover:text-red-dark" href="/">
-                          <span class="hidden">Like</span>
-                          <i class="fa fa-heart"></i>
-                      </a>
-                  </footer>
-
-                  </article>
+              <Card id={1} ServiceName={"Web Development"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink = {"/"} Description={"To explore this service please join us!"} />
+              <Card id={1} ServiceName={"Web Development"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink = {"/"} Description={"To explore this service please join us!"} />
+              <Card id={1} ServiceName={"Web Development"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink = {"/"} Description={"To explore this service please join us!"} />
+              <Card id={1} ServiceName={"Web Development"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink = {"/"} Description={"To explore this service please join us!"} />
                 
-              </div>        
           </div>
       </div>
     </div>
