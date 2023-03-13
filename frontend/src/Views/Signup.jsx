a2bbbc481a20756826547acba50dddfb81210761
tatvkamdar79 <kamdartatv79@gmail.com>
2023-03-13 21:01:02 +0530

    Signup landing page done

--- frontend/src/Views/Signup.jsx
+++ frontend/src/Views/Signup.jsx
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import client from "../Assets/client.png";
 import freelancer from "../Assets/freelancer.png";
 const Signup = () => {
-  const [btn, setBtn] = useState("Create Account");
+  const options = {
+    default: "Create Account",
+    client: "Signup as a Client",
+    freelancer: "Apply as a Freelancer",
+  };
+  const [btn, setBtn] = useState("default");
 
   const setTypeOfUser = (e) => {
     console.log(e.target.id);
@@ -10,64 +15,68 @@ const Signup = () => {
   };
 
   return (
-    <div className="h-[80vh] sm:h-[60vh] max-w-[800px] flex flex-col justify-self-center items-center place-content-center m-auto">
-      <h1 className="font-playfair font-semibold text-5xl text-center my-6 p-4">
-        Join as a client or freelancer
-      </h1>
-      <div className="z-50 w-full flex flex-col sm:flex-row gap-3 my-4 font-playfair font-semibold text-xl justify-center items-center sm:justify-evenly">
-        {/* Client Card */}
-        <button
-          onClick={setTypeOfUser}
-          id="client"
-          className="flex h-full justify-center items-center max-w-[400px] px-6 sm:w-5/12 py-4 border-4 border-blue rounded-xl"
-        >
-          <img id="client" src={client} alt="client" width={140} />
-          <div id="client" className="flex flex-col h-full justify-evenly">
-            <input
-              type="radio"
-              id="client"
-              className="relative cursor-pointer w-[30px] h-[18px] left-[85%] bottom-2"
-              value={"client"}
-            />
-            <p
-              id="client"
-              className="text-center flex place-items-center h-full"
-            >
-              I'm a client, hiring for a project
-            </p>
-          </div>
-        </button>
-        {/* Freelancer Card */}
-        <button
-          id="freelancer"
-          onClick={setTypeOfUser}
-          className="flex h-full justify-center items-center max-w-[400px] px-6 sm:w-5/12 py-4 border-4 border-blue rounded-xl"
-        >
-          <img id="freelancer" src={freelancer} alt="client" width={140} />
-          <div id="freelancer" className="flex flex-col h-full justify-evenly">
-            <input
-              type="radio"
-              name="typeOfUser"
+    <div className="h-screen bg-white">
+      <div className="h-[80vh] sm:h-[60vh] max-w-[800px] flex flex-col justify-self-center items-center place-content-center m-auto">
+        <h1 className="font-playfair font-semibold text-5xl text-center my-6 p-4">
+          Join as a client or freelancer
+        </h1>
+        <div className="z-50 w-full flex flex-col sm:flex-row gap-3 my-4 font-playfair font-semibold text-xl justify-center items-center sm:justify-evenly">
+          {/* Client Card */}
+          <button
+            onClick={setTypeOfUser}
+            id="client"
+            className={`flex h-full justify-center items-center max-w-[400px] px-6 sm:w-5/12 py-4 border-4  rounded-xl transition duration-500 ease-out ${
+              btn === "client"
+                ? "bg-green-500 border-gray-900"
+                : "bg-sky-100 border-blue"
+            }`}
+          >
+            <img id="client" src={client} alt="client" width={140} />
+            <div id="client" className="flex flex-col h-full justify-evenly">
+              <p
+                id="client"
+                className="text-center flex place-items-center h-full"
+              >
+                I'm a client, hiring for a project
+              </p>
+            </div>
+          </button>
+          {/* Freelancer Card */}
+          <button
+            id="freelancer"
+            onClick={setTypeOfUser}
+            className={`flex h-full justify-center items-center max-w-[400px] px-6 sm:w-5/12 py-4 border-4  rounded-xl transition duration-500 ease-in-out ${
+              btn === "freelancer"
+                ? "bg-green-500 border-gray-900"
+                : "bg-sky-100 border-blue"
+            }`}
+          >
+            <img id="freelancer" src={freelancer} alt="client" width={140} />
+            <div
               id="freelancer"
-              className="relative cursor-pointer w-[30px] h-[18px] left-[85%] bottom-2"
-              value={"freelancer"}
-            />
-            <p
-              id="freelancer"
-              className="text-center flex place-items-center h-full"
+              className="flex flex-col h-full justify-evenly"
             >
-              I'm a freelancer, looking for work
-            </p>
-          </div>
-        </button>
+              <p
+                id="freelancer"
+                className="text-center flex place-items-center h-full"
+              >
+                I'm a freelancer, looking for work
+              </p>
+            </div>
+          </button>
+        </div>
+        <a href={`${btn === "default" ? "#" : "/signup/" + btn}`}>
+          <button
+            disabled={btn === "Create Account"}
+            className={`bg-black text-white ${
+              btn === "default" ? "opacity-25" : ""
+            } transition duration-500 ease-in-out border-2 border-black text-center w-[280px] h-[45px] rounded-xl font-semibold text-xl font-playfair`}
+            onClick={() => console.log("clicking")}
+          >
+            {options[btn]}
+          </button>
+        </a>
       </div>
-      <a
-        className={`bg-blue ${
-          btn === "Create Account" ? "opacity-25" : ""
-        } border-2 border-black text-center w-[250px] h-[40px] rounded-xl font-semibold text-xl font-playfair`}
-      >
-        {btn}
-      </a>
     </div>
   );
 };
