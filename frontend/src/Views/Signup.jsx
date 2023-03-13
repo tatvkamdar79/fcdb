1f88364a54191f88a9eee4f9d0fed55824899088
Tatva Kamdar <kamdartatv1@gmail.com>
2023-03-13 20:03:11 +0530

    mr2

--- frontend/src/Views/Signup.jsx
+++ frontend/src/Views/Signup.jsx
@@ -3,61 +3,68 @@ import client from "../Assets/client.png";
 import freelancer from "../Assets/freelancer.png";
 const Signup = () => {
   const [btn, setBtn] = useState("Create Account");
-  const [route, setRoute] = useState("");
-  const [disabled, SetDisabled] = useState(false);
-  const optionChange = (e) => {
-    SetDisabled(true);
-    console.log(e.target.value);
-    setRoute(e.target.value);
-    setBtn("Join as a " + e.target.value);
+
+  const setTypeOfUser = (e) => {
+    console.log(e.target.id);
+    setBtn(e.target.id);
   };
-  const clicked = () => {
-    console.log("clicked");
-  };
+
   return (
-    <div className="h-[90vh] w-full sm:w-5/6 flex flex-col justify-self-center items-center place-content-center m-auto">
+    <div className="h-[80vh] sm:h-[60vh] max-w-[800px] flex flex-col justify-self-center items-center place-content-center m-auto">
       <h1 className="font-playfair font-semibold text-5xl text-center my-6 p-4">
         Join as a client or freelancer
       </h1>
-      <div className="w-full flex flex-col sm:flex-row gap-3 my-4 font-playfair font-semibold text-xl justify-center items-center sm:justify-evenly">
+      <div className="z-50 w-full flex flex-col sm:flex-row gap-3 my-4 font-playfair font-semibold text-xl justify-center items-center sm:justify-evenly">
         {/* Client Card */}
-        <div className="flex h-full justify-center items-center max-w-[400px] sm:w-5/12 py-4 border-4 border-blue rounded-xl">
-          <img src={client} alt="client" width={140} />
-          <div className="flex flex-col h-full justify-evenly">
+        <button
+          onClick={setTypeOfUser}
+          id="client"
+          className="flex h-full justify-center items-center max-w-[400px] px-6 sm:w-5/12 py-4 border-4 border-blue rounded-xl"
+        >
+          <img id="client" src={client} alt="client" width={140} />
+          <div id="client" className="flex flex-col h-full justify-evenly">
             <input
               type="radio"
-              name="typeOfUser"
-              id=""
+              id="client"
               className="relative cursor-pointer w-[30px] h-[18px] left-[85%] bottom-2"
               value={"client"}
-              onClick={optionChange}
             />
-            <p className="text-center">I'm a client, hiring for a project</p>
+            <p
+              id="client"
+              className="text-center flex place-items-center h-full"
+            >
+              I'm a client, hiring for a project
+            </p>
           </div>
-        </div>
-        {/* Client Card */}
-        <div className="flex h-full justify-center items-center max-w-[400px] px-6 sm:w-5/12 py-4 border-4 border-blue rounded-xl">
-          <img src={freelancer} alt="client" width={140} />
-          <div className="flex flex-col h-full justify-evenly">
+        </button>
+        {/* Freelancer Card */}
+        <button
+          id="freelancer"
+          onClick={setTypeOfUser}
+          className="flex h-full justify-center items-center max-w-[400px] px-6 sm:w-5/12 py-4 border-4 border-blue rounded-xl"
+        >
+          <img id="freelancer" src={freelancer} alt="client" width={140} />
+          <div id="freelancer" className="flex flex-col h-full justify-evenly">
             <input
               type="radio"
               name="typeOfUser"
-              id=""
+              id="freelancer"
               className="relative cursor-pointer w-[30px] h-[18px] left-[85%] bottom-2"
               value={"freelancer"}
-              onClick={optionChange}
             />
-            <p className="text-center">I'm a freelancer, looking for work</p>
+            <p
+              id="freelancer"
+              className="text-center flex place-items-center h-full"
+            >
+              I'm a freelancer, looking for work
+            </p>
           </div>
-        </div>
+        </button>
       </div>
       <a
         className={`bg-blue ${
           btn === "Create Account" ? "opacity-25" : ""
         } border-2 border-black text-center w-[250px] h-[40px] rounded-xl font-semibold text-xl font-playfair`}
-        disabled={true}
-        onClick={clicked}
-        href={`signup/${route}`}
       >
         {btn}
       </a>
