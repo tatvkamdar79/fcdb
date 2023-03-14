f62476b72498ab9ec3ebb570f68f1c28938ce1ec
Aman Jolly <aman.amanjolly@gmail.com>
2023-03-14 16:09:02 +0530

    khjo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import Home from "./Views/Home";
 import Signup from "./Views/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpForm from "./Views/SignUpForm";
+import SignInForm from "./Views/SignInForm";
+
 // import Routing from "./Views/Routing";
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <Route path={"/home"} element={<Home />} />
         <Route path={"/signup"} element={<Signup />} />
         <Route path={"/signup/client"} element={<SignUpForm />} />
+        <Route path={"/signin/client"} element={<SignInForm />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Views/SignInForm.jsx
+++ frontend/src/Views/SignInForm.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Users = [
   {
@@ -16,20 +16,12 @@ const Users = [
   },
 ];
 
-const SignUpForm = () => {
+const SignInForm = () => {
   const initialFormDetails = {
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
   };
   const [formDetails, setFormDetails] = useState(initialFormDetails);
-
-  useEffect(() => {
-    if (!localStorage.getItem("users")) {
-      localStorage.setItem("users", JSON.stringify(Users));
-    }
-  }, []);
 
   const changeDetails = (e) => {
     let updatedValue = { ...formDetails };
@@ -39,23 +31,12 @@ const SignUpForm = () => {
 
   async function handleStaticSubmit(e) {
     e.preventDefault();
-    console.log(formDetails);
     let usersList = JSON.parse(localStorage.getItem("users"));
-    console.log(usersList);
-    let isUserRegistered = usersList.filter(
-      (user) => user.email == formDetails.email
+    const user = usersList.filter(
+      (user) =>
+        user.email == formDetails.email && user.password == formDetails.password
     );
-    if (
-      isUserRegistered.length == 0 &&
-      formDetails.password == formDetails.confirmPassword
-    ) {
-      const newUser = {
-        email: formDetails.email,
-        password: formDetails.password,
-      };
-      const newUsersList = [...usersList, newUser];
-      localStorage.setItem("users", JSON.stringify(newUsersList));
-    }
+    console.log(user.length > 0 ? "Signed In" : "Invalid Credentials");
   }
 
   async function handleSubmit(e) {
@@ -86,23 +67,6 @@ const SignUpForm = () => {
         </div>
         <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
           <form onSubmit={handleStaticSubmit}>
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700 undefined"
-              >
-                Name
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="text"
-                  name="name"
-                  value={formDetails.name}
-                  onChange={changeDetails}
-                  className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                />
-              </div>
-            </div>
             <div className="mt-4">
               <label
                 htmlFor="email"
@@ -137,51 +101,11 @@ const SignUpForm = () => {
                 />
               </div>
             </div>
-            <div className="mt-4">
-              <label
-                htmlFor="confirmPassword"
-                className="block text-sm font-medium text-gray-700 undefined"
-              >
-                Confirm Password
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  onChange={changeDetails}
-                  value={formDetails.confirmPassword}
-                  className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                />
-              </div>
-            </div>
             <a href="#" className="text-xs text-purple-600 hover:underline">
               Forgot Password?
             </a>
             <div className="flex items-center mt-4">
-              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                Register
-              </button>
-            </div>
-          </form>
-          <div className="mt-4 text-grey-600">
-            Already have an account?{" "}
-            <span>
-              <a
-                className="text-purple-600 hover:underline"
-                href="/signin/client"
-              >
-                Log in
-              </a>
-            </span>
-          </div>
-          <div className="flex items-center w-full my-4">
-            <hr className="w-full" />
-            <p className="px-3 ">OR</p>
-            <hr className="w-full" />
-          </div>
-          <div className="my-6 space-y-2">
-            <button
-              aria-label="Login with Google"
+              <button className="w-full px-4 py-2 tracking"
               type="button"
               className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
             >
@@ -190,12 +114,7 @@ const SignUpForm = () => {
                 viewBox="0 0 32 32"
                 className="w-5 h-5 fill-current"
               >
-                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-              </svg>
-              <p>Login with Google</p>
-            </button>
-            <button
-              aria-label="Login with GitHub"
+                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.
               role="button"
               className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
             >
@@ -215,4 +134,4 @@ const SignUpForm = () => {
   );
 };
 
-export default SignUpForm;
+export default SignInForm;
