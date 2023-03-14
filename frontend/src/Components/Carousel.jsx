a9b33bd52e700dc4fdf578e3d9a9529f6c44f55d
hrithik <hrithikjainDB@gmail.com>
2023-03-14 10:14:47 +0530

    Carousel with multiple gigs

--- frontend/src/Components/Carousel.jsx
+++ frontend/src/Components/Carousel.jsx
@@ -26,29 +26,33 @@ const scrollRight = () => {
           <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
         </div>
         <div>
-          <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
+          <Card id={1} ServiceName={"Logo Design"} Description={"Build your brand!"} ImageLink={"https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"} RedirectLink={"/"}/>
+        </div>
+        <div>
+          <Card id={1} ServiceName={"Word Press"} Description={"Customize your site!"} ImageLink={"https://www.dreamhost.com/blog/wp-content/uploads/2022/09/What-is-WordPress-Feature-750x499.jpg"} RedirectLink={"/"}/>
+        </div>
+        <div>
+          <Card id={1} ServiceName={"SEO"} Description={"Unlock growth online!"} ImageLink={"https://blog.hubspot.com/hubfs/what%20is%20seo.jpg"} RedirectLink={"/"}/>
+        </div>
+        <div>
+          <Card id={1} ServiceName={"Data Entry"} Description={"Learn your business!"} ImageLink={"https://contentstatic.timesjobs.com/img/61836079/Master.jpg"} RedirectLink={"/"}/>
         </div>
         <div>
           <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
         </div>
         <div>
-          <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
+          <Card id={1} ServiceName={"Logo Design"} Description={"Build your brand!"} ImageLink={"https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"} RedirectLink={"/"}/>
         </div>
         <div>
-          <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
+          <Card id={1} ServiceName={"Word Press"} Description={"Customize your site!"} ImageLink={"https://www.dreamhost.com/blog/wp-content/uploads/2022/09/What-is-WordPress-Feature-750x499.jpg"} RedirectLink={"/"}/>
         </div>
         <div>
-          <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
+          <Card id={1} ServiceName={"SEO"} Description={"Unlock growth online!"} ImageLink={"https://blog.hubspot.com/hubfs/what%20is%20seo.jpg"} RedirectLink={"/"}/>
         </div>
         <div>
-          <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
+          <Card id={1} ServiceName={"Data Entry"} Description={"Learn your business!"} ImageLink={"https://contentstatic.timesjobs.com/img/61836079/Master.jpg"} RedirectLink={"/"}/>
         </div>
-        <div>
-          <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
-        </div>
-        <div>
-          <Card id={1} ServiceName={"Web Development"} Description={"Web development is coool!"} ImageLink={"https://www.analyticsinsight.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg"} RedirectLink={"/"}/>
-        </div>
+        
         
       </div>
     </div>
