fb08339ccd7a3e3123572296760ed9aafbd7fb1c
Aman Jolly <aman.amanjolly@gmail.com>
2023-03-14 16:18:51 +0530

    Sign up/Sign in with local storage

--- frontend/src/Views/SignInForm.jsx
+++ frontend/src/Views/SignInForm.jsx
@@ -105,7 +105,30 @@ const SignInForm = () => {
               Forgot Password?
             </a>
             <div className="flex items-center mt-4">
-              <button className="w-full px-4 py-2 tracking"
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                Register
+              </button>
+            </div>
+          </form>
+          <div className="mt-4 text-grey-600">
+            Create an account?{" "}
+            <span>
+              <a
+                className="text-purple-600 hover:underline"
+                href="/signup/client"
+              >
+                Sign up
+              </a>
+            </span>
+          </div>
+          <div className="flex items-center w-full my-4">
+            <hr className="w-full" />
+            <p className="px-3 ">OR</p>
+            <hr className="w-full" />
+          </div>
+          <div className="my-6 space-y-2">
+            <button
+              aria-label="Login with Google"
               type="button"
               className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
             >
@@ -114,7 +137,12 @@ const SignInForm = () => {
                 viewBox="0 0 32 32"
                 className="w-5 h-5 fill-current"
               >
-                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.
+                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+              </svg>
+              <p>Login with Google</p>
+            </button>
+            <button
+              aria-label="Login with GitHub"
               role="button"
               className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
             >
