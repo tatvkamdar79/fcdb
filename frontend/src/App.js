672f6d2136f56d926512f2d2bdd88704f76bd218
Tatva Kamdar <kamdartatv1@gmail.com>
2023-03-14 16:22:43 +0530

    SignupForm Coonflict resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import Home from "./Views/Home";
 import Signup from "./Views/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ClientSignup from "./Views/ClientSignup";
 import Carousel from "./Components/Carousel";
 import SignUpForm from "./Views/SignUpForm";
 import PostSignUpModal from "./Components/PostSignUpModal";
@@ -13,8 +12,8 @@ function App() {
         <Route path={"/"} element={<Home />} />
         <Route path={"/home"} element={<Home />} />
         <Route path={"/signup"} element={<Signup />} />
-        <Route path={"/signup/client"} element={<ClientSignup />} />
-        <Route path={"/test/Carousel"} element={<Carousel/>}/>
+        <Route path={"/signup/client"} element={<SignUpForm />} />
+        <Route path={"/test/Carousel"} element={<Carousel />} />
 
         <Route path={"/signup/client"} element={<SignUpForm />} />
         <Route path={"/signup/freelancer"} element={<SignUpForm />} />
