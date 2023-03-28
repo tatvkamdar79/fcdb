c3954e30546455ff09d6a7886ca0735d646478b9
Tatva Kamdar <kamdartatv1@gmail.com>
2023-03-28 11:39:03 +0530

    Did lot of work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
 import Signup from "./Views/Signup";
-import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  useNavigate,
+  Outlet,
+  Navigate,
+  Link,
+} from "react-router-dom";
 import SignUpForm from "./Views/SignUpForm";
 import SignInForm from "./Views/SignInForm";
 // import PostSignUpModal from "./Components/PostSignUpModal";
@@ -8,61 +15,123 @@ import Landing from "./Views/Landing";
 import CategoryPage from "./Views/CategoryPage";
 import AdPage from "./Views/AdPage";
 import { createContext, useContext, useEffect, useState } from "react";
-import { read_cookie } from "sfcookies";
 import Home from "./Views/Home";
+import { getCookie, setCookie } from "./Hooks/useCookies";
+import axios from "axios";
+import Chat from "./Views/Chat";
 
-const userData = createContext({});
+const UserContext = createContext({});
+
 function App() {
   const navigate = useNavigate();
-  const [user, setUser] = useState({}); //{ name: "TATV" });
+  const [user, setUser] = useState({ loggedIn: false });
+
   useEffect(() => {
-    console.log(document.cookie.split("="));
-    if (document.cookie.split("=").length > 0) {
-      // api call here
-      setUser({ name: "TATV" });
-      navigate("/home");
+    async function getUserDetails() {
+      let jwt = getCookie("JWT_AUTH");
+      if (jwt.length == 0) {
+        return;
+      }
+
+      const headers = {
+        authorization: `Bearer ${jwt}`,
+      };
+      const response = await axios.post(
+        "http://localhost:8080/api/getUserDetails",
+        { token: getCookie("JWT_AUTH") },
+        {
+          headers,
+        }
+      );
+      console.log("Got data on first fetch", response.data.data);
+      const fetchedData = response.data.data;
+      fetchedData["loggedIn"] = true;
+      setUser(fetchedData);
+      return;
+    }
+    console.log("reached here");
+    if (!user.loggedIn) {
+      getUserDetails();
+      console.log(user);
     }
   }, []);
+
   return (
     <>
-      <Navbar />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Navbar />
+        <Link to={"/home"} className="border-2 border-black px-3">
+          Home
+        </Link>
+        <Link to={"/signin"} className="border-2 border-black px-3">
+          Signin
+        </Link>
+        <Routes>
+          <Route path={"/"} element={<Landing />} />
 
-      <Routes>
-        <Route path={"/"} element={<Landing />} />
-      </Routes>
-
-      {Object.keys(user).length > 0 ? (
-        // {/* User Signed In Accesible Routes */}
-        <userData.Provider value={user}>
-          <Routes>
+          {/* User Authenticated Routes */}
+          <Route element={<UserAuthContext user={user} />}>
+            <Route path={"/"} element={<Home />} />
             <Route path={"/home"} element={<Home />} />
-            {/* Individual Category and Ad Page */}
-            <Route
-              path={"/categories/:categoryName"}
-              element={<CategoryPage />}
-            />
-            <Route
-              path={"/categories/:categoryName/:id"}
-              element={<AdPage />}
-            />
-          </Routes>
-        </userData.Provider>
-      ) : (
-        // navigate("/signin")
-        <div>Not Logged In</div>
-      )}
-      <Routes>
-        {/* Signup Routes */}
-        <Route path={"/signup"} element={<Signup />} />
-        <Route path={"/signup/client"} element={<SignUpForm />} />
-        <Route path={"/signup/freelancer"} element={<SignUpForm />} />
+          </Route>
+          {/* User Authenticated Routes */}
+          <Route
+            path={"/categories/:categoryName"}
+            element={<CategoryPage />}
+          />
+          <Route path={"/categories/:categoryName/:id"} element={<AdPage />} />
+          <Route
+            path={"/categories/:categoryName/:id/chat"}
+            element={<Chat />}
+          />
 
-        {/* Signin Routes */}
-        <Route path={"/signin"} element={<SignInForm />} />
-      </Routes>
+          {/* Signup Routes */}
+          <Route path={"/signup"} element={<Signup />} />
+          <Route path={"/signup/client"} element={<SignUpForm />} />
+          <Route path={"/signup/freelancer"} element={<SignUpForm />} />
+
+          {/* Signin Routes */}
+          <Route path={"/signin"} element={<SignInForm />} />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
 
+function UserAuthContext({ user }) {
+  return user.loggedIn ? <Outlet /> : <Navigate to={"/signin"} />;
+}
+
 export default App;
-export { userData };
+export { UserContext };
+
+// const login = async () => {
+//   const jwt = getCookie("JWT_AUTH");
+//   alert(jwt);
+//   if (jwt.length !== 0) {
+//     console.log(jwt);
+//   }
+//   const fetchedUser = await getUserDetails();
+//   fetchedUser["loggedIn"] = true;
+//   setUser(fetchedUser);
+//   console.log(user);
+//   navigate("/home");
+// };
+// const logout = () => {
+//   const jwt = getCookie("JWT_AUTH");
+//   if (jwt.length !== 0) {
+//     setCookie("JWT_AUTH", "", 0);
+//   }
+//   setUser({ loggedIn: false });
+//   console.log(user);
+// };
+// useEffect(() => {
+//   async function checkIfLoggedIn() {
+//     let jwt = getCookie("JWT_AUTH");
+//     console.log(user);
+//     if (jwt.length !== 0) {
+//       await login();
+//     }
+//   }
+//   let d = checkIfLoggedIn();
+// }, []);
--- frontend/src/Views/AdPage.jsx
+++ frontend/src/Views/AdPage.jsx
@@ -1,15 +1,130 @@
-import React from "react";
-import { useLocation } from "react-router";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useLocation, useParams } from "react-router";
+import { BiUserCircle, BiRupee } from "react-icons/bi";
+import { BsClock } from "react-icons/bs";
+import { FiRefreshCcw } from "react-icons/fi";
+import { FcCheckmark } from "react-icons/fc";
+import { RiStarSFill } from "react-icons/ri";
 
 const AdPage = () => {
-  const { state } = useLocation();
-  console.log(state);
+  const { categoryName, id } = useParams();
+  let { state } = useLocation();
+  const ad = { ...state };
+  console.log(ad);
+  const freelancer = {
+    id: 1,
+    name: "Tatva Kamdar",
+    about:
+      "My name is tatv and I like to build projects. I do my work really well and I give deadlines only when I know I can compelete them My name is tatv and I like to build projects. I do my work really well and I give deadlines only when I know I can compelete them.",
+    rating: 5,
+  };
+  // state["freelancer"] = freelancer;
+  let stars = [];
+  for (let i = 0; i < freelancer.rating; i++) {
+    stars.push(i);
+  }
   return (
-    <div>
-      <p>{state.id}</p>
-      <p>{state.user}</p>
-      <p>{state.description}</p>
-      <p>{state.price}</p>
+    <div className="flex flex-col md:flex-row w-5/6 justify-center md:justify-between mx-auto">
+      <div className="flex flex-col">
+        <p>FCDB</p>
+        <div className="flex flex-col pr-16 justify-center mx-auto md:mx-0">
+          <p className="font-semibold text-3xl my-3">{ad.title}</p>
+          <p className="flex place-items-center text-lg font-semibold font-playfair gap-x-2 my-2">
+            <BiUserCircle size={23} />
+            {ad.user}
+          </p>
+          <div className="flex justify-center place-items-center bg-gray-200 my-4">
+            <img
+              src={ad.link}
+              alt="Ad Img"
+              className="my-3 max-h-[500px]"
+              width={800}
+              height={900}
+            />
+          </div>
+          <div>
+            <p className="text-3xl font-semibold text-gray-700 underline">
+              About This Ad
+            </p>
+            <p className="text-md my-2">{ad.aboutAd.repeat(5)}</p>
+          </div>
+        </div>
+        <br />
+        <div className="w-11/12">
+          <p className="my-2 font-semibold font-serif underline text-lg text-gray-800">
+            About the Seller
+          </p>
+          {/* Profile Img and profile Page of Freelancer */}
+          <Link to={"/freelancer/" + freelancer.id} className="flex w-fit">
+            <img
+              src="https://source.unsplash.com/random/80x80"
+              alt="Freelancer Image"
+              className="rounded-full my-2"
+            />
+            <div className="flex flex-col place-items-start justify-center px-4">
+              <p>{freelancer.name}</p>
+              <p className="flex place-items-center">
+                {stars.map((id) => (
+                  <RiStarSFill key={id} />
+                ))}
+                <span className="px-1">{freelancer.rating}</span>
+              </p>
+            </div>
+          </Link>
+          <p className="text-gray-600 mt-1">{freelancer.about}</p>
+        </div>
+      </div>
+      {/* Purchase Card */}
+      <div className="flex justify-center mt-20 md:mt-0">
+        <div className="border border-gray-400 rounded-sm w-[500px] h-fit relative top-10">
+          <p className="flex justify-between place-items-center font-serif text-lg px-5 py-3">
+            <span>{ad.title}</span>
+            <span>
+              <BiRupee size={20} className="inline mb-1" />
+              {ad.price}
+            </span>
+          </p>
+          <div className="flex text-justify px-5 my-4">
+            <p className="text-md text-gray-600 font-serif">
+              {ad.aboutAd.repeat(2)}
+            </p>
+          </div>
+          <div className="flex justify-between place-items-center font-serif text-sm px-5 py-2">
+            <p className="flex place-items-center">
+              <BsClock size={17} className="mx-2" />2 Days Delivery
+            </p>
+            <p className="flex">
+              <FiRefreshCcw size={17} className="mx-2" />3 Revisions
+            </p>
+          </div>
+          <div className="flex flex-col px-5 py-3 text-gray-500">
+            <p className="flex place-items-center w-[100px] gap-x-1">
+              <FcCheckmark size={20} />
+              tag 1
+            </p>
+            <p className="flex place-items-center w-[100px] gap-x-1">
+              <FcCheckmark size={20} />
+              tag 2
+            </p>
+            <p className="flex place-items-center w-[100px] gap-x-1">
+              <FcCheckmark size={20} />
+              tag 3
+            </p>
+            <p className="flex place-items-center w-[100px] gap-x-1">
+              <FcCheckmark size={20} />
+              tag 4
+            </p>
+          </div>
+          <Link
+            to={`/categories/${categoryName}/${id}/chat`}
+            state={{ ...state, freelancer }}
+            className="flex justify-center bg-green-500 w-11/12 mx-auto font-serif text-center text-xl text-white opacity-90 py-1 my-3"
+          >
+            Continue
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
