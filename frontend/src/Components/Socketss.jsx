c8cc233d32bddba5ca9cb406465c44893cbe302a
Aman Jolly <aman.amanjolly@gmail.com>
2023-04-03 10:28:25 +0530

    Added socket

---
+++ frontend/src/Components/Socketss.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import createSocket from "../utils/socket";
+
+const Socketss = () => {
+  const socket = createSocket();
+  useEffect(() => {
+    console.log("In here");
+    socket.on("recieveMessage", (data) => {
+      console.log(`I recievied a ot of ${data}`);
+    });
+  }, []);
+
+  return <div>Hellooo</div>;
+};
+
+export default Socketss;
