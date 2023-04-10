0423a3db3844cb23c7ba8af15279e7e24db32fd2
Tatva Kamdar <kamdartatv1@gmail.com>
2023-04-10 18:09:56 +0530

    Ran tests

--- backend/controllers/getUserDetails.js
+++ backend/controllers/getUserDetails.js
@@ -1,3 +1,4 @@
+const conversation = require("../models/conversationSchema");
 const utils = require("../utils/response");
 // const Conversation = require("../models/conversationSchema");
 
@@ -6,22 +7,22 @@ module.exports = async (req, res) => {
   console.log("accessed getUserDetails");
   console.log(req.user);
   let data = [];
-  // let conversations = await Conversation.find({
-  //   $or: [{ clientId: req.user._id }, { freelancerId: req.user._id }],
-  // });
-  // for (let conversation of conversations) {
-  //   let newObj = {
-  //     adId: conversation.adId,
-  //     clientId: conversation.clientId,
-  //     freelancerId: conversation.freelancerId,
-  //   };
-  //   let messages = [];
-  //   for (let message of conversation.messages) {
-  //     messages.push(message);
-  //   }
-  //   newObj["messages"] = messages;
-  //   data.push(newObj);
-  // }
+  let conversations = await conversation.find({
+    $or: [{ clientId: req.user._id }, { freelancerId: req.user._id }],
+  });
+  for (let conversation of conversations) {
+    let newObj = {
+      adId: conversation.adId,
+      clientId: conversation.clientId,
+      freelancerId: conversation.freelancerId,
+    };
+    let messages = [];
+    for (let message of conversation.messages) {
+      messages.push(message);
+    }
+    newObj["messages"] = messages;
+    data.push(newObj);
+  }
   let result = {
     user: req.user,
     conversations: [],
